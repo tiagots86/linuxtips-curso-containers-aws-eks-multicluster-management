e7f2570bd8589c57ec4678287646e5cee3ede5cd
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-05-03 10:53:26 -0300

    cluster control plane

---
+++ control-plane/data.tf
@@ -0,0 +1,17 @@
+data "aws_ssm_parameter" "vpc" {
+  name = var.ssm_vpc
+}
+
+data "aws_ssm_parameter" "subnets" {
+  count = length(var.ssm_subnets)
+  name  = var.ssm_subnets[count.index]
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = aws_eks_cluster.main.id
+}
+
+data "aws_ssm_parameter" "karpenter_ami" {
+  count = length(var.karpenter_capacity)
+  name  = var.karpenter_capacity[count.index].ami_ssm
+}
