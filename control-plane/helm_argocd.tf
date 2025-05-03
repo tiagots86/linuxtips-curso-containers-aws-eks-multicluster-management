8f472601ed53319130bd18af8167d7f498052ac6
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-05-03 10:56:10 -0300

    helm argocd

---
+++ control-plane/helm_argocd.tf
@@ -0,0 +1,55 @@
+resource "helm_release" "argocd" {
+
+  name             = "argocd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  set {
+    name  = "server.extraArgs[0]"
+    value = "--insecure"
+  }
+
+  set {
+    name = "server.service.type"
+    value = "NodePort"
+  }
+
+  set {
+    name  = "server.extensions.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "server.enable.proxy.extension"
+    value = "true"
+  }
+
+  set {
+    name  = "server.extensions.image.repository"
+    value = "quay.io/argoprojlabs/argocd-extension-installer"
+  }
+
+  set {
+    name  = "server.extensions.extensionList[0].name"
+    value = "rollout-extension"
+  }
+
+  set {
+    name  = "server.extensions.extensionList[0].env[0].name"
+    value = "EXTENSION_URL"
+  }
+
+  set {
+    name  = "server.extensions.extensionList[0].env[0].value"
+    value = "https://github.com/argoproj-labs/rollout-extension/releases/download/v0.3.6/extension.tar"
+  }
+
+  depends_on = [
+    aws_eks_cluster.main,
+    helm_release.karpenter
+  ]
+
+}
